5d84899593b86f547cf07e2f07fa54d7f39d855d
Ross Hadden <rosshadden@gmail.com>
2016-12-11 01:27:18 -0500

    Added a second controllable marine model.

--- app/states/example-state.js
+++ app/states/example-state.js
@@ -12,8 +12,7 @@ export class ExampleState extends Phaser.State {
 	layer = null;
 
 	create() {
-		this.physics.startSystem(Phaser.Physics.ARCADE);
-
+		window.state = this;
 		this.map = this.add.tilemap('example-map');
 		this.map.addTilesetImage('background');
 		this.map.setCollision([ 1 ]);
@@ -24,6 +23,7 @@ export class ExampleState extends Phaser.State {
 
 		this.player = new Player(this.game, PLAYER.DEFAULT_X, PLAYER.DEFAULT_Y);
 		window.player = this.player;
+		this.player2 = new Player(this.game, PLAYER.DEFAULT_X + 90, PLAYER.DEFAULT_Y + 90);
 		this.game.trigger(STATE_EVENTS.EXAMPLE_COMPLETED);
 
 		this.buttons = {
@@ -31,6 +31,10 @@ export class ExampleState extends Phaser.State {
 			down: this.input.keyboard.addKey(Phaser.Keyboard.DOWN),
 			left: this.input.keyboard.addKey(Phaser.Keyboard.LEFT),
 			right: this.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
+			k: this.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_8),
+			j: this.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_2),
+			h: this.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_4),
+			l: this.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_6),
 		};
 
 		this.clock = new THREE.Clock();
@@ -38,6 +42,16 @@ export class ExampleState extends Phaser.State {
 	}
 
 	update() {
+		if (this.buttons.up.isDown) this.player.y -= 1;
+		if (this.buttons.down.isDown) this.player.y += 1;
+		if (this.buttons.left.isDown) this.player.x -= 1;
+		if (this.buttons.right.isDown) this.player.x += 1;
+
+		if (this.buttons.k.isDown) this.player2.y -= 1;
+		if (this.buttons.j.isDown) this.player2.y += 1;
+		if (this.buttons.h.isDown) this.player2.x -= 1;
+		if (this.buttons.l.isDown) this.player2.x += 1;
+
 		if (this.marine.rotation) {
 			let rotation = 0;
 			let anyDown = false;
@@ -48,11 +62,6 @@ export class ExampleState extends Phaser.State {
 					rotation += b * Math.PI / 2;
 				}
 			});
-
-			if (this.buttons.up.isDown) this.player.y -= 1;
-			if (this.buttons.down.isDown) this.player.y += 1;
-			if (this.buttons.left.isDown) this.player.x -= 1;
-			if (this.buttons.right.isDown) this.player.x += 1;
 
 			if (!anyDown) rotation = this.marine.rotation.y;
 			this.marine.rotation.y = rotation;
@@ -67,17 +76,42 @@ export class ExampleState extends Phaser.State {
 			}
 		}
 
+		if (this.marine2.rotation) {
+			let rotation = 0;
+			let anyDown = false;
+
+			[ 'k', 'h', 'j', 'l' ].forEach((button, b) => {
+				if (this.buttons[button].isDown) {
+					anyDown = true;
+					rotation += b * Math.PI / 2;
+				}
+			});
+
+			if (!anyDown) rotation = this.marine2.rotation.y;
+			this.marine2.rotation.y = rotation;
+
+			if (anyDown && this.marine2.mixer.clipAction('idle').isRunning()) {
+				this.marine2.stopAll();
+				this.marine2.play('walk', 1);
+			}
+			if (!anyDown && this.marine2.mixer.clipAction('walk').isRunning()) {
+				this.marine2.stopAll();
+				this.marine2.play('idle', 1);
+			}
+		}
+
 		let dt = this.clock.getDelta();
 		if (this.marine.mixer) this.marine.update(dt);
-
-		this.physics.arcade.collide(this.player, this.layer);
+		if (this.marine2.mixer) this.marine2.update(dt);
 	}
 
 	render() {
 		this.game.debug.body(this.player);
+		this.game.debug.body(this.player2);
 
 		this.renderer.render(this.scene, this.camera);
 		this.player.setTexture(PIXI.Texture.fromCanvas(document.getElementsByTagName('canvas')[1]));
+		this.player2.setTexture(PIXI.Texture.fromCanvas(document.getElementsByTagName('canvas')[1]));
 	}
 
 	three() {
@@ -90,7 +124,12 @@ export class ExampleState extends Phaser.State {
 		this.camera = new THREE.OrthographicCamera(-width, width, height, -height, 1, 1000);
 
 		this.marine = new THREE.BlendCharacter();
+		this.marine2 = new THREE.BlendCharacter();
 		window.marine = this.marine;
+		window.marine2 = this.marine2;
+
+		const origin = { x: -width, y: height };
+
 		this.marine.load('assets/models/marine/marine_anims_core.json', () => {
 			const radius = this.marine.geometry.boundingSphere.radius;
 			this.camera.position.set(0.0, radius, radius * 3.5);
@@ -98,10 +137,22 @@ export class ExampleState extends Phaser.State {
 
 			this.marine.rotation.x = Math.PI / 4;
 			this.marine.rotation.y = Math.PI;
-			this.marine.position.x = -width + radius / 2;
-			this.marine.position.y = height - radius / 2;
+			this.marine.position.x = origin.x + radius / 2;
+			this.marine.position.y = origin.y - radius / 2;
 			this.marine.stopAll();
 			this.marine.play('idle', 1);
+		});
+
+		this.marine2.load('assets/models/marine/marine_anims_core.json', () => {
+			const radius = this.marine2.geometry.boundingSphere.radius;
+			this.scene.add(this.marine2);
+
+			this.marine2.rotation.x = Math.PI / 4;
+			this.marine2.rotation.y = Math.PI;
+			this.marine2.position.x = origin.x + radius * 2;
+			this.marine2.position.y = origin.y - radius / 2;
+			this.marine2.stopAll();
+			this.marine2.play('idle', 1);
 		});
 
 		this.camera.position.z = 5;
